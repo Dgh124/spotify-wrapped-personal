a77a98082c57f69ae14291545087d0ddfde00376
Krishin Wadhwani <krishin901@gmail.com>
2024-10-22 01:10:03 -0400

    implemented login, profile details, top tracks

--- spotify_wrapped/Spotify.py
+++ spotify_wrapped/Spotify.py
@@ -1,8 +1,12 @@
 import base64
 import os, requests, time
 import urllib.parse
+from typing import TypedDict
 
-from dotenv import load_dotenv, dotenv_values
+from dotenv import load_dotenv
+load_dotenv()
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
 
 class Artist:
     def __init__(self, name, id,  image, genre):
@@ -16,75 +20,69 @@ class Track:
         self.album_image = album_image
         self.song_name = song_name
 
+class User:
+    def __init__(self, display_name, _id, pfp, product, uri):
+        self.display_name = display_name
+        self.id = _id
+        self.pfp = pfp
+        self.product = product
+        self.uri = uri
 
-load_dotenv()
-client_id = os.getenv("CLIENT_ID")
-client_secret = os.getenv("CLIENT_SECRET")
-auth_code = None
-access_token = None
-refresh_token = None
-expires_at = 0
-# data = {
-#     "grant_type" : "authorization_code",
-#     "client_id" : client_id,
-#     "client_secret" : client_secret,
-#     'code' : auth_code,
-#     'redirect_uri': redirect_uri
-# }
-redirect_uri = "https://localhost:8000"
-headers = {
-    "Authorization": f"Bearer {access_token}"
-}
+redirect_uri = "http://localhost:8000/auth"
+
 
 def get_auth_url():
-    redirect_url = 'https://localhost:8000'
-    # Define the scope you need (e.g., user-read-private)
-    scope = 'user-read-private user-read-email user-top-read'
-
     # Construct the authorization URL
     auth_url = 'https://accounts.spotify.com/authorize'
+
+    # Gives access to user profile and top tracks, albums, etc.
+    scope = 'user-read-private user-read-email user-top-read'
     params = {
         'client_id': client_id,
         'response_type': 'code',
-        'redirect_uri': redirect_url,
+        'redirect_uri': redirect_uri,
         'scope': scope
     }
 
     full_auth_url = f"{auth_url}?{urllib.parse.urlencode(params)}"
     return full_auth_url
 
-def get_requests(url):
-    response = requests.get(url, headers=headers)
-    user_json = response.json()
-    return user_json
 
-def is_token_expired():
-    current_time = time.time()
-    return current_time >= expires_at
-
-def get_access_token():
-    token_url = "https://accounts.spotify.com/api/token"
-    data_t = {
+def get_access_token(auth_code:str) -> (str, int, str):
+    """
+    Takes auth_code and returns Spotify api access token
+    :param auth_code: auth code returned by get_auth_url called at /login slug
+    :return: (access_token, expire_time, refresh_token)
+    """
+    url = "https://accounts.spotify.com/api/token"
+    data = {
         "grant_type": "authorization_code",
         "client_id": client_id,
         "client_secret": client_secret,
         'code': auth_code,
         'redirect_uri': redirect_uri
     }
-    token_headers = {
-        "Content-Type": "application/x-www-form-urlencoded"
-    }
-    token_response = requests.post(token_url, data=data_t, headers=token_headers)
-    token_json_response = token_response.json()
-    access_token1= token_json_response.get("access_token")
-    refresh_token1 = token_json_response.get("refresh_token")
-    print("Access_token", access_token1)
-    print("Refresh_token", refresh_token1)
-
-def refresh_access_token():
     client_creds = f"{client_id}:{client_secret}"
     client_creds_b64 = base64.b64encode(client_creds.encode()).decode()
+    headers = {
+        "Authorization": f"Basic {client_creds_b64}",
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    res = requests.post(url, data=data, headers=headers)
+    json = res.json()
+
+    access_token:str = json.get("access_token")
+    expires_in: int = json["expires_in"]
+    expire_time: int = int(time.time()) + expires_in
+    refresh_token:str = json.get("refresh_token")
+
+    return access_token, expire_time, refresh_token
+
+
+def refresh_access_token(refresh_token:str):
     token_url = "https://accounts.spotify.com/api/token"
+    client_creds = f"{client_id}:{client_secret}"
+    client_creds_b64 = base64.b64encode(client_creds.encode()).decode()
     headers = {
         'Authorization': f'Basic {client_creds_b64}',
         'Content-Type': 'application/x-www-form-urlencoded'
@@ -94,40 +92,86 @@ def refresh_access_token():
         'refresh_token': refresh_token
     }
     response = requests.post(token_url, headers=headers, data=data)
-    token = response.json()
-    expires_in = token.get('expires_in', 3600)
-    expires_at = time.time() + expires_in
-    access_token = token.get('access_token')
+    json = response.json()
 
-def get_top_artist():
-    if access_token is None and refresh_token is None:
-        get_access_token()
-    if is_token_expired():
-        refresh_access_token()
-    url = "https://api.spotify.com/v1/me/top/artists?time_range=long_term&limit=10"
-    response = get_requests(url)
-    top_artists = []
-    for item in response["items"]:
-        artist = Artist(item['name'], item['id'], item['images'][0]['url'], item['genres'])
-        top_artists.append(artist)
-    return top_artists
+    access_token:str = json.get('access_token')
+    expires_in = json.get('expires_in', 3600)
+    expires_at = int(time.time()) + expires_in
+    refresh_token:str = json.get('refresh_token')
 
-def get_top_track():
+    return access_token, expires_at, refresh_token
+
+
+def get_requests(url, access_token):
+    headers = {"Authorization": f"Bearer {access_token}"}
+    response = requests.get(url, headers=headers)
+    json = response.json()
+    return json
+
+
+def is_token_expired(expires_at:int) -> bool:
+    current_time = time.time()
+    return current_time >= expires_at
+
+
+def has_access(access_token:str, expires_at:int, refresh_token:str):
     if access_token is None and refresh_token is None:
-        get_access_token()
-    if is_token_expired():
-        refresh_access_token()
-    url = "https://api.spotify.com/v1/me/top/tracks?time_range=long_term&limit=10"
-    response = get_requests(url)
-    print(response)
+        return False
+    if is_token_expired(expires_at):
+        refresh_access_token(refresh_token)
+
+    return True
+
+
+def get_top_artists(access_token, expires_at, refresh_token,):
+    if not has_access(access_token, expires_at, refresh_token):
+        return {"status":"error", "reason":"noToken"}
+
+    url = f"https://api.spotify.com/v1/me/top/artists?time_range=long_term"
+    response = get_requests(url, access_token)
+
+    top_artists = [
+        Artist(item['name'], item['id'], item['images'][0]['url'], item['genres'])
+        for item in response["items"]
+    ]
+    return {"status":"success", "value":top_artists}
+
+
+def get_top_tracks(access_token, expires_at, refresh_token,):
+    if not has_access(access_token, expires_at, refresh_token):
+        return {"status":"error", "reason":"noToken"}
+
+    url = f"https://api.spotify.com/v1/me/top/tracks?time_range=long_term"
+    response = get_requests(url, access_token)
+
     top_tracks = []
-    for item in response['items']:
-        track = Track(item['album']['name'], item['album'][0]['url'], item['name'])
-        top_tracks.append(track)
-    return top_tracks
+    for item in response["items"]:
+        top_tracks.append(
+            Track(item['album']['name'], item['album'][0]['url'], item['name'])
+        )
 
-# print(get_auth_url())
-# print(get_access_token())
+    return {"status":"success", "value":top_tracks}
 
-for artist in get_top_artist():
-    print(artist.name)
+def get_user(access_token, expires_at, refresh_token):
+    """
+    Gets user details from Spotify API and returns User object with attributes
+    :param access_token:
+    :param expires_at:
+    :param refresh_token:
+    :return: display_name, uid, pfp, product, uri
+    """
+    if not has_access(access_token, expires_at, refresh_token):
+        return {"status":"error", "reason":"noToken"}
+
+    url = "https://api.spotify.com/v1/me"
+    response = get_requests(url, access_token)
+
+    user = User(
+        display_name = response.get("display_name", None),
+        _id= response.get("id", None),
+        pfp = response.get('images') and response['images'][0].get('url') if response['images'] else None,
+        product = response.get("product", None),
+        uri = response.get("uri", None),
+    )
+    return {"status":"success", "value":user}
+
--- spotify_wrapped/views.py
+++ spotify_wrapped/views.py
@@ -1,4 +1,48 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from spotify_wrapped.Spotify import get_auth_url, get_access_token, get_top_tracks, get_user
+
 
 def index(request):
-    return render(request, "spotify_wrapped/home.html", {})
+    access_token = request.session.get("access_token", None)
+    expire_time = request.session.get("expire_time", None)
+    refresh_token = request.session.get("refresh_token", None)
+
+    # If not logged in yet, show base home page
+    if access_token is None or expire_time is None or refresh_token is None:
+        return render(request, "spotify_wrapped/home.html", {})
+
+    top_tracks_result = get_top_tracks(
+        access_token=access_token, expires_at=expire_time, refresh_token=refresh_token,
+    )
+    user_result = get_user(
+        access_token=access_token, expires_at=expire_time, refresh_token=refresh_token,
+    )
+
+    # So far, this only means the user is not logged in, so log them in
+    if top_tracks_result["status"] == "error":
+        HttpResponseRedirect(reverse('spotify_wrapped:login'))
+    if user_result["status"] == "error":
+        return HttpResponseRedirect(reverse('spotify_wrapped:login'))
+
+    top_tracks = top_tracks_result["value"]
+    user = user_result["value"]
+    return render(request, "spotify_wrapped/home.html",
+                  {"top_tracks": top_tracks, "user": user})
+
+def login(request):
+    return HttpResponseRedirect(get_auth_url())
+
+def auth(request):
+    auth_code = request.GET.get('code')
+    access_token, expire_time, refresh_token = get_access_token(auth_code)
+
+    request.session['access_token'] = access_token
+    request.session['expire_time'] = expire_time
+    request.session['refresh_token'] = refresh_token
+    return HttpResponseRedirect(reverse('spotify_wrapped:index'))
+
+
+
